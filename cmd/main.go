ed63e39aa7bbe111266fc3b2e01dbe4976fb8c02
sauce-git <sauce@garam.sh>
2026-08-17 14:34:55 +0900

    feat: add a stub agent reconciler

--- cmd/main.go
+++ cmd/main.go
@@ -18,6 +18,9 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	agentv1alpha1 "github.com/garamsh/gagent-operator/api/v1alpha1"
+	"github.com/garamsh/gagent-operator/internal/controller"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -29,6 +32,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(agentv1alpha1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -158,6 +162,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err := (&controller.AgentReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "Failed to create controller", "controller", "agent")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
